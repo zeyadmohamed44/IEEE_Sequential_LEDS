7d8db09550c4c5ee61e881c5d495e52da1338b51
zeyadmohamed44 <zeyadmohamed200404@gmail.com>
2024-09-09 19:00:38 +0300

    Create STD_TYPES.h

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,23 @@
+/*
+* Author: Zeyad mohamed
+*Date: 29/8/2024
+* version: V01
+*
+*/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+
+
+
+typedef 	u8     		 Std_ReturnType ;
+
+#define	 	E_OK 		(Std_ReturnType)1
+#define 	E_NOT_OK 	(Std_ReturnType)0
+
+
+
+#endif
