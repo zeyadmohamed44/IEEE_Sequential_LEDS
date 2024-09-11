969a8ea0fbdad1469fb1ebf832861f639cb5790b
zeyadmohamed44 <zeyadmohamed200404@gmail.com>
2024-09-11 18:38:06 +0300

    Update GPIO_PRIVATE.h

--- MCAL/GPIO_PRIVATE.h
+++ MCAL/GPIO_PRIVATE.h
@@ -15,20 +15,20 @@
 
 #define GPIOA_CRL *((volatile u32*)(GPIO_PORTA + 0x00))
 #define GPIOA_CRH *((volatile u32*)(GPIO_PORTA + 0x04))
-#define GPIOA_IDR *((volatile u32*)(GPIO_PORTA + 0x88))
-#define GPIOA_ODR *((volatile u32*)(GPIO_PORTA + 0x8c))
+#define GPIOA_IDR *((volatile u32*)(GPIO_PORTA + 0x08))
+#define GPIOA_ODR *((volatile u32*)(GPIO_PORTA + 0x0c))
 
 
 #define GPIOB_CRL *((volatile u32*)(GPIO_PORTB + 0x00))
 #define GPIOB_CRH *((volatile u32*)(GPIO_PORTB + 0x04))
-#define GPIOB_IDR *((volatile u32*)(GPIO_PORTB + 0x88))
-#define GPIOB_ODR *((volatile u32*)(GPIO_PORTB + 0x8c))
+#define GPIOB_IDR *((volatile u32*)(GPIO_PORTB + 0x08))
+#define GPIOB_ODR *((volatile u32*)(GPIO_PORTB + 0x0c))
 
 
 #define GPIOC_CRL *((volatile u32*)(GPIO_PORTC + 0x00))
 #define GPIOC_CRH *((volatile u32*)(GPIO_PORTC + 0x04))
-#define GPIOC_IDR *((volatile u32*)(GPIO_PORTC + 0x88))
-#define GPIOC_ODR *((volatile u32*)(GPIO_PORTC + 0x8c))
+#define GPIOC_IDR *((volatile u32*)(GPIO_PORTC + 0x08))
+#define GPIOC_ODR *((volatile u32*)(GPIO_PORTC + 0x0c))
 
 
 #endif
